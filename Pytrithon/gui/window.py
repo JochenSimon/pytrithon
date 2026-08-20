ed539d1c5284431e0898cec141b1fae55ef84f13
Jochen Simon <jochen.simon7@gmail.com>
2026-08-20 10:07:18 +0200

    fixed window moveevent; added two new kernels to clock

--- Pytrithon/gui/window.py
+++ Pytrithon/gui/window.py
@@ -50,6 +50,7 @@ class Window(QDialog):
   def moveEvent(self, event):
     if hasattr(self, "socket") and self.socket.outputs("pos"):
       self.socket.put("pos", (self.frameGeometry().left(), self.frameGeometry().top()))
+    super().moveEvent(event)  
 
   def mousePressEvent(self, event):
     if self.moveable:
